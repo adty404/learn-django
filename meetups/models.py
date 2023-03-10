08fac153206c2c2a6adee8ff41b3768f91df2356
Aditya Prasetyo <aditya.prasetyo@enseval.com>
2023-03-10 11:13:22 +0700

    many-to-many Relations

--- meetups/models.py
+++ meetups/models.py
@@ -12,12 +12,21 @@ class Location(models.Model):
         return f'{self.name} ({self.address})'
 
 
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.email
+
+
 class Meetup(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
-    location = models.ForeignKey(Location, on_delete=models.CASCADE, related_name='meetups')
+    location = models.ForeignKey(
+        Location, on_delete=models.CASCADE, related_name='meetups')
+    participants = models.ManyToManyField(Participant, blank=True, null=True)
 
     # function to control what showing up or what we get as a result if we need this instance as a string
     def __str__(self):
