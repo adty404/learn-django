33fc310f6fe94d0f4bbdeb5eacc43854b1277b38
Aditya Prasetyo <adty404@gmail.com>
2023-03-12 11:44:32 +0700

    From Modelform to Form

--- meetups/forms.py
+++ meetups/forms.py
@@ -4,6 +4,4 @@ from .models import Participant
 
 
 class RegistrationForm(forms.ModelForm):
-    class Meta:
-        model = Participant
-        fields = ['email']
+    email = forms.EmailField(label='Your email')
