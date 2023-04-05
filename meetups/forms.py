3210fe224ba4bbf991f4cdba24e96edcc97bf36f
Aditya Prasetyo <aditya.prasetyo@enseval.com>
2023-04-05 09:22:10 +0700

    From Modelform to Form (FIXED)

--- meetups/forms.py
+++ meetups/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 
-from .models import Participant
 
-
-class RegistrationForm(forms.ModelForm):
+class RegistrationForm(forms.Form):
     email = forms.EmailField(label='Your email')
+
+# example with a ModelForm
+# class RegistrationForm(forms.ModelForm):
+#     class Meta:
+#         model = Participant
+#         fields = ['email']
