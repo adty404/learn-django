13447dcbef77aaa3e833b894962c6d1ebc8fd5ad
Aditya Prasetyo <aditya.prasetyo@enseval.com>
2023-04-05 08:39:53 +0700

    Polishing Organizer Email

--- meetups/views.py
+++ meetups/views.py
@@ -5,6 +5,7 @@ from .forms import RegistrationForm
 
 # Create your views here.
 
+
 def index(request):
     meetups = Meetup.objects.all()
 
@@ -22,10 +23,10 @@ def meetup_details(request, meetup_slug):
             registration_form = RegistrationForm(request.POST)
             if registration_form.is_valid():
                 user_email = registration_form.cleaned_data['email']
-                participant, _ = Participant.objects.get_or_create(email=user_email)
+                participant, _ = Participant.objects.get_or_create(
+                    email=user_email)
                 selected_meetup.participants.add(participant)
-                return redirect('confirm-registration')
-
+                return redirect('confirm-registration', meetup_slug=meetup_slug)
 
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found': True,
@@ -33,9 +34,14 @@ def meetup_details(request, meetup_slug):
             'form': registration_form
         })
     except Exception as exc:
+        print(exc)
         return render(request, 'meetups/meetup-details.html', {
             'meetup_found': False
         })
 
-def confirm_registration(request):
-    return render(request, 'meetups/registration-success.html')
+
+def confirm_registration(request, meetup_slug):
+    meetup = Meetup.objects.get(slug=meetup_slug)
+    return render(request, 'meetups/registration-success.html', {
+        'organizer_email': meetup.organizer_email
+    })
